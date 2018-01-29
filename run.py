7d5a17a13427686c27bf4f150ff7bb54cb8f99cd
PgTower <pgtowerdev@gmail.com>
2018-01-29 18:23:19 -0500

    Removed Local Port 5000 in run.py

--- run.py
+++ run.py
@@ -10,5 +10,6 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    db.create_all()
+    db.session.commit()
+    app.run(debug=True)
